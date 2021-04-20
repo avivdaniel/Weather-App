7418ab47b561775eb87f4f9699bf487ab0d5079f
Aviv Daniel <aviv@techmarketing.co.il>
2021-04-20 12:57:16 +0300

    reformat code at Favorites component

--- src/favorites/Favorites.jsx
+++ src/favorites/Favorites.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+
 import FavoriteCard from '@/components/FavoriteCard';
 
 const Favorites = () => {
-  const {favorites} = useSelector(state => state);
+  const { favorites } = useSelector((state) => state);
 
   return (
     <ul className="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 space-y-4">
-      {favorites.length > 0 && favorites.map(({localizedName, locationKey})=> {
-        return <FavoriteCard key={locationKey} localizedName={localizedName} locationKey={locationKey} className="bg-blue-200"/>
-      })}
+      {favorites.length > 0 &&
+        favorites.map(({ localizedName, locationKey }) => {
+          return (
+            <FavoriteCard
+              key={locationKey}
+              localizedName={localizedName}
+              locationKey={locationKey}
+              className="bg-blue-200"
+            />
+          );
+        })}
     </ul>
   );
 };
