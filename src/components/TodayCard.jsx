5dd42fd0a8574c1b41fb9382f8af810fc221cdd4
Aviv Daniel <aviv@techmarketing.co.il>
2021-04-20 13:05:59 +0300

    reformat code at TodayCard

--- src/components/TodayCard.jsx
+++ src/components/TodayCard.jsx
@@ -3,23 +3,38 @@ import { cn } from '@/utils';
 
 import Icon from '@/components/Icon';
 
-const TodayCard = ({day = {}, localizedName, children, className}) => {
-
-  const  {
+const TodayCard = ({ day = {}, localizedName, children, className }) => {
+  const {
     WeatherText: description,
     WeatherIcon: iconNum,
-    Temperature: temp
+    Temperature: temp,
   } = day;
 
   return (
     <div className={cn('text-gray-600 flex flex-col', className)}>
-      {localizedName && <h1 className="text-6xl font-extrabold tracking-wide text-center">{localizedName}</h1>}
+      {localizedName && (
+        <h1 className="text-6xl font-extrabold tracking-wide text-center">
+          {localizedName}
+        </h1>
+      )}
 
       <div className="flex-1 flex items-center justify-center">
         <div className="relative flex flex-col justify-center items-center">
-          {description && <span className="font-bold text-2xl block self-start pl-2">{description}</span>}
-          {iconNum && <Icon number={iconNum} className="w-48 h-48 lg:w-72 lg:h-72"/>}
-          {temp && <span className="text-xl font-bold absolute bottom-0 right-2">{temp?.Metric?.Value + '°C'}</span>}
+          {description && (
+            <span className="font-bold text-2xl block self-start pl-2">
+              {description}
+            </span>
+          )}
+
+          {iconNum && (
+            <Icon number={iconNum} className="w-48 h-48 lg:w-72 lg:h-72" />
+          )}
+
+          {temp && (
+            <span className="text-xl font-bold absolute bottom-0 right-2">
+              {temp?.Metric?.Value + '°C'}
+            </span>
+          )}
         </div>
       </div>
 
