8448b2cbb90894e55bee5212ca7eb1c8135f8bdb
Aviv Daniel <aviv@techmarketing.co.il>
2021-04-20 12:59:06 +0300

    reformat code at DayCard

--- src/components/DayCard.jsx
+++ src/components/DayCard.jsx
@@ -1,36 +1,34 @@
 import React from 'react';
 import Icon from '@/components/Icon';
 
-const weekDayList = [
-  "Sun",
-  "Mon",
-  "Tue",
-  "Wed",
-  "Thu",
-  "Fri",
-  "Sat",
-]
+const weekDayList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const DayCard = ({day: {Temperature:temperature, Day:day, Date:date, ...props}}) => {
-
+const DayCard = ({
+  day: { Temperature: temperature, Day: day, Date: date, ...props },
+}) => {
   const formatDayOfWeek = (date) => {
     if (!date) return;
     const weekDay = new Date(date).getDay();
     return weekDayList[weekDay];
-  }
+  };
 
-  const { Maximum: {Value: maxTemp}, Minimum: {Value: minTemp}} = temperature;
+  const {
+    Maximum: { Value: maxTemp },
+    Minimum: { Value: minTemp },
+  } = temperature;
   const { IconPhrase: description, Icon: dayIconNumber } = day;
-  // const { Icon: nightIconNumber } = night;
 
   return (
     <div className="px-4 border-b border-gray-100 py-8 text-white flex justify-between xl:border-l xl:border-b-0 xl:flex-col xl:justify-between xl:items-center xl:h-30">
       <h3 className="font-bold xl:text-4xl tracking-wider">
-         {formatDayOfWeek(date)}
+        {formatDayOfWeek(date)}
       </h3>
 
       <div className="pl-2 flex items-center xl:flex-col xl:pl-0">
-        <Icon number={dayIconNumber} className="h-6 w-6 xl:h-16 xl:w-16 xl:py-4"/>
+        <Icon
+          number={dayIconNumber}
+          className="h-6 w-6 xl:h-16 xl:w-16 xl:py-4"
+        />
         <span className="text-center ml-4 xl:ml-0 xl:text-xl">
           {description}
           <span className="block xl:ml-0 xl:text-xl">{`${minTemp} - ${maxTemp} °C`}</span>
