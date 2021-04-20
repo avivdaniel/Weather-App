f17d2279642604b43d315b8ee68a40307d853cd0
Aviv Daniel <aviv@techmarketing.co.il>
2021-04-20 13:08:29 +0300

    :fire: unused services

--- src/services/services.js
+++
@@ -1,7 +0,0 @@
-import accuWeatherApi from '@/client.js';
-import {apiKey} from '@/config';
-
-export const getCityOptions = async (query) => {
-  const { data } = await accuWeatherApi.get(`/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${query}`)
-  return data;
-}
