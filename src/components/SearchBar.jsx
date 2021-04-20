94452846bd9412cc7fca5f9eeb12ead00ada3739
Aviv Daniel <aviv@techmarketing.co.il>
2021-04-20 12:07:05 +0300

    :recycle: error handeling for searchbar autocomplete

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,218 +4,66 @@ import debounce from 'lodash.debounce';
 import {useDispatch} from 'react-redux';
 import { getFiveDayWeatherAsync } from '@/redux/cityForecastSlice';
 import {getCityOptions} from '@/home/service';
+import {cleanErrors, receiveErrors} from '@/redux/errorsSlice';
 
 const DEFAULT_CITY = 'Tel Aviv';
 
 const SearchBar = ({}) => {
   const dispatch = useDispatch();
 
-  const [cityOptions, setCityOptions] = useState([]);
   const [selectedCity, setSelectedCity] = useState({});
   const [loading, setLoading] = useState(false);
-  const [query, setQuery] = useState('');
 
-useEffect(()=> {
-  (async () => {
-    // const [defalutCityOption] = await getCityOptions(DEFAULT_CITY);
+  useEffect(()=> {
+    (async () => {
+      try {
+        setLoading(true);
+        dispatch(cleanErrors());
+        const [defaultCityOption] = await getCityOptions(DEFAULT_CITY);
 
-    let defalutCityOption = {
-      "Version": 1,
-      "Key": "215854",
-      "Type": "City",
-      "Rank": 31,
-      "LocalizedName": "Tel Aviv",
-      "Country": {
-      "ID": "IL",
-        "LocalizedName": "Israel"
-    },
-      "AdministrativeArea": {
-      "ID": "TA",
-        "LocalizedName": "Tel Aviv"
-    }}
+        setSelectedCity({label:defaultCityOption?.LocalizedName, value: defaultCityOption?.Key});
 
-    setSelectedCity({label:defalutCityOption?.LocalizedName, value: defalutCityOption?.Key})
+        dispatch(getFiveDayWeatherAsync({
+          locationKey: defaultCityOption?.Key,
+          localizedName: defaultCityOption?.LocalizedName
+        }));
 
-    dispatch(getFiveDayWeatherAsync({
-      locationKey: defalutCityOption?.Key,
-      localizedName: defalutCityOption?.LocalizedName
-    }))
-  })();
-}, []);
+      } catch (error) {
+        dispatch(receiveErrors({
+          error: error.message
+        }))
+        console.error(error);
+      }
+      setLoading(false);
+    })();
+  }, []);
 
   const debounceFetchCityOptions = debounce(async (inputText, cb) => {
-  setLoading(true);
-  try {
-  const data = await getCityOptions(inputText);
-  //   const data = [
-  //     {
-  //       "Version": 1,
-  //       "Key": "178087",
-  //       "Type": "City",
-  //       "Rank": 10,
-  //       "LocalizedName": "Berlin",
-  //       "Country": {
-  //         "ID": "DE",
-  //         "LocalizedName": "Germany"
-  //       },
-  //       "AdministrativeArea": {
-  //         "ID": "BE",
-  //         "LocalizedName": "Berlin"
-  //       }
-  //     },
-  //     {
-  //       "Version": 1,
-  //       "Key": "334472",
-  //       "Type": "City",
-  //       "Rank": 65,
-  //       "LocalizedName": "Berlin",
-  //       "Country": {
-  //         "ID": "US",
-  //         "LocalizedName": "United States"
-  //       },
-  //       "AdministrativeArea": {
-  //         "ID": "NH",
-  //         "LocalizedName": "New Hampshire"
-  //       }
-  //     },
-  //     {
-  //       "Version": 1,
-  //       "Key": "111584",
-  //       "Type": "City",
-  //       "Rank": 75,
-  //       "LocalizedName": "Berlin",
-  //       "Country": {
-  //         "ID": "CO",
-  //         "LocalizedName": "Colombia"
-  //       },
-  //       "AdministrativeArea": {
-  //         "ID": "TOL",
-  //         "LocalizedName": "Tolima"
-  //       }
-  //     },
-  //     {
-  //       "Version": 1,
-  //       "Key": "111585",
-  //       "Type": "City",
-  //       "Rank": 75,
-  //       "LocalizedName": "Berlin",
-  //       "Country": {
-  //         "ID": "CO",
-  //         "LocalizedName": "Colombia"
-  //       },
-  //       "AdministrativeArea": {
-  //         "ID": "TOL",
-  //         "LocalizedName": "Tolima"
-  //       }
-  //     },
-  //     {
-  //       "Version": 1,
-  //       "Key": "111738",
-  //       "Type": "City",
-  //       "Rank": 75,
-  //       "LocalizedName": "Berlin",
-  //       "Country": {
-  //         "ID": "CO",
-  //         "LocalizedName": "Colombia"
-  //       },
-  //       "AdministrativeArea": {
-  //         "ID": "VAU",
-  //         "LocalizedName": "Vaupés"
-  //       }
-  //     },
-  //     {
-  //       "Version": 1,
-  //       "Key": "111888",
-  //       "Type": "City",
-  //       "Rank": 75,
-  //       "LocalizedName": "Berlin",
-  //       "Country": {
-  //         "ID": "CR",
-  //         "LocalizedName": "Costa Rica"
-  //       },
-  //       "AdministrativeArea": {
-  //         "ID": "A",
-  //         "LocalizedName": "Alajuela"
-  //       }
-  //     },
-  //     {
-  //       "Version": 1,
-  //       "Key": "220397",
-  //       "Type": "City",
-  //       "Rank": 75,
-  //       "LocalizedName": "Berlin",
-  //       "Country": {
-  //         "ID": "JM",
-  //         "LocalizedName": "Jamaica"
-  //       },
-  //       "AdministrativeArea": {
-  //         "ID": "11",
-  //         "LocalizedName": "Saint Elizabeth"
-  //       }
-  //     },
-  //     {
-  //       "Version": 1,
-  //       "Key": "131206",
-  //       "Type": "City",
-  //       "Rank": 75,
-  //       "LocalizedName": "Berlin",
-  //       "Country": {
-  //         "ID": "SV",
-  //         "LocalizedName": "El Salvador"
-  //       },
-  //       "AdministrativeArea": {
-  //         "ID": "US",
-  //         "LocalizedName": "Usulután"
-  //       }
-  //     },
-  //     {
-  //       "Version": 1,
-  //       "Key": "2174864",
-  //       "Type": "City",
-  //       "Rank": 75,
-  //       "LocalizedName": "Berlin",
-  //       "Country": {
-  //         "ID": "US",
-  //         "LocalizedName": "United States"
-  //       },
-  //       "AdministrativeArea": {
-  //         "ID": "NJ",
-  //         "LocalizedName": "New Jersey"
-  //       }
-  //     },
-  //     {
-  //       "Version": 1,
-  //       "Key": "336534",
-  //       "Type": "City",
-  //       "Rank": 75,
-  //       "LocalizedName": "Berlin",
-  //       "Country": {
-  //         "ID": "US",
-  //         "LocalizedName": "United States"
-  //       },
-  //       "AdministrativeArea": {
-  //         "ID": "WI",
-  //         "LocalizedName": "Wisconsin"
-  //       }
-  //     }
-  //   ]
+    try {
+      dispatch(cleanErrors());
+      setLoading(true);
+      const data = await getCityOptions(inputText);
+      cb(data.map((city) => ({label: `${city?.LocalizedName} - ${city?.Country?.LocalizedName}`, value: city?.Key})))
+    } catch (error) {
+      dispatch(receiveErrors({
+        error: error.message
+      }))
+      console.error(error);
+    }
+    setLoading(false);
+  }, 1000)
 
-  cb(data.map((city) => ({label: `${city?.LocalizedName} - ${city?.Country?.LocalizedName}`, value: city?.Key})))
-  } catch (err) {
-    console.error(err);
+  const handleChange = selectedCityOption => {
+    if (!selectedCityOption) return;
+    setSelectedCity(selectedCityOption);
+    dispatch(getFiveDayWeatherAsync({
+      localizedName: selectedCityOption.label,
+      locationKey: selectedCityOption.value
+    }));
   }
-  setLoading(false);
-}, 1000)
-
-const handleChange = selectedCityOption => {
-  setSelectedCity(selectedCityOption);
-  dispatch(getFiveDayWeatherAsync({
-    localizedName: selectedCityOption.label,
-    locationKey: selectedCityOption.value
-  }));
-}
 
   return (
+    <div className="container mx-auto">
     <AsyncSelect
       cacheOptions
       value={selectedCity}
@@ -224,7 +72,9 @@ const handleChange = selectedCityOption => {
       loadOptions={debounceFetchCityOptions}
       onChange={handleChange}
       loading={loading}
+      isDisabled={loading}
     />
+    </div>
   );
 };
 
