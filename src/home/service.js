a8330d827ab726b758e955b6d7c55bbcb8dde0f6
Aviv Daniel <aviv@techmarketing.co.il>
2021-04-20 13:07:32 +0300

    reformat code at home services

--- src/home/service.js
+++ src/home/service.js
@@ -1,23 +1,12 @@
 import accuWeatherApi from '@/client.js';
-import axios from 'axios';
 
-import {apiKey} from '@/config';
+import { apiKey } from '@/config';
 
 export const getCityOptions = async (query) => {
-  try  {
-  const { data } = await accuWeatherApi.get(`/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${query}`)
-  return data;
-  } catch (err) {
-    if (err && err.message === 'Network Error') {
-      throw new Error('Opps... you may pass the daily limit');
-    }
-    throw err;
-  }
-}
-
-export const getFiveDayWeather = async (locationKey) => {
   try {
-    const { data } =  await accuWeatherApi.get(`/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}&metric=${true}`);
+    const { data } = await accuWeatherApi.get(
+      `/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${query}`
+    );
     return data;
   } catch (err) {
     if (err && err.message === 'Network Error') {
@@ -25,4 +14,18 @@ export const getFiveDayWeather = async (locationKey) => {
     }
     throw err;
   }
-}
+};
+
+export const getFiveDayWeather = async (locationKey) => {
+  try {
+    const { data } = await accuWeatherApi.get(
+      `/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}&metric=${true}`
+    );
+    return data;
+  } catch (err) {
+    if (err && err.message === 'Network Error') {
+      throw new Error('Opps... you may pass the daily limit');
+    }
+    throw err;
+  }
+};
